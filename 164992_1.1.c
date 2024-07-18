a0aa511e07594f9846ce4813fc0c3b55dd3ffa01
Rushabh Goswami <Rushabhgiri.Goswami@einfochips.com>
2024-07-18 15:16:36 +0530

    1.1

---
+++ 164992_1.1.c
@@ -0,0 +1,12 @@
+/*
+*This program is just for Experiment with leaving out parts of the Hello World! program, to see what error messages we get.
+*Author: Rushabh Goswami
+*Created: 18/7/2024
+*/
+
+//Required libraries
+#include<stdio.h>
+void main(){
+	printf("Hello World!\n");
+	return;
+}	
