c73dfcb73e95be5fb5fef951ed2bd68d31029779
Rushabh Goswami <Rushabhgiri.Goswami@einfochips.com>
2024-07-18 18:18:39 +0530

    1.2 & 1.3

---
+++ chapter1/164992_1.2.c
@@ -0,0 +1,13 @@
+/*
+*Experiment to find out what happens when prints's argument string contains \c, where c is some character not listed above.
+*Author: Rushabh Goswami
+*Created: 18/7/2024
+*/
+
+//Required Libraries
+#include<stdio.h>
+
+void main(){
+	printf("Rushabh\cgiri Goswami");
+	return;
+}
