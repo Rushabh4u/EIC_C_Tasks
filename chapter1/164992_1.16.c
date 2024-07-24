716f0840b6e9313d898b2466c4ef1d3485375bdc
Rushabh Goswami <Rushabhgiri.Goswami@einfochips.com>
2024-07-24 16:38:29 +0530

    Makefile and error handeling files added

---
+++ chapter1/164992_1.16.c
@@ -0,0 +1,13 @@
+/*
+*AIM: Revise the main routine of the longest-line program so it will correctly print the length of arbitrary long input lines, and as much as possible of the text.
+*Author: Rushabh Goswami
+*Created: 24/7/2024
+*/
+
+//Required Libraries
+#include<stdio.h>
+
+int main(){
+	
+	
+}
